27f1ac032bd5fe6906f75f4e41c5247ceb382611
neverland <chenjiahan.jait@bytedance.com>
2026-08-20 14:47:33 +0800

    chore(deps): upgrade Rslint to v0.8.1

--- rslint.config.ts
+++ rslint.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig, js, ts } from '@rslint/core';
+import { defineConfig, globals, js, ts } from '@rslint/core';
 
 export default defineConfig([
   js.configs.recommended,
   ts.configs.recommended,
   {
-    files: ['playground/**/*', 'test/**/*'],
+    files: ['playground/**/*'],
+    rules: {
+      'no-undef': 'off',
+    },
+  },
+  {
+    files: ['**/*.test.{ts,tsx}'],
+    languageOptions: {
+      globals: globals.rstest,
+    },
+  },
+  {
+    files: ['test/**/src/index.js'],
     rules: {
       'no-undef': 'off',
     },
